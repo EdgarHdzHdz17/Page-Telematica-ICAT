e43cfd627d164c34f2b75b2b416f52e5251840ff
Edgar Hernández Hernández <edgarhdzhdz17@hotmail.com>
2024-07-11 18:07:40 -0600

    Delete antologias directory

--- antologias/biodiversidad/narrativas/especies/narrativa/js/eventos.js
+++
@@ -1,7 +0,0 @@
-function especies_endemicas(){
-    var elemento = document.getElementById("img-intro");
-        elemento.src = "img/especies_endemicas.jpg";
-}
-function megadiverso_mexico() {
-    var elemento = document.getElementById('especies').pause();
-}
